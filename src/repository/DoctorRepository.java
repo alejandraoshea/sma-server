f02ae3ac635447b321fdf858df611fafc7b411a2
alejandraoshea <alejandra.osheafernandez@usp.ceu.es>
2025-10-16 09:46:17 +0200

    initial structure

--- src/repository/DoctorRepository.java
+++ src/repository/DoctorRepository.java
@@ -1,4 +1,13 @@
 package repository;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class DoctorRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public DoctorRepository(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
 }
