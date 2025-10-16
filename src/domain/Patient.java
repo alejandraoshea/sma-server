2fdd30ded734dd720d5524613a0d34bbe21a9aa2
alejandraoshea <alejandra.osheafernandez@usp.ceu.es>
2025-10-16 15:30:46 +0200

    added patient attributes

--- src/domain/Patient.java
+++ src/domain/Patient.java
@@ -17,18 +17,20 @@ public class Patient {
     private Gender gender;
     private LocalDate birthDate;
     private Set<Symptom> symptoms;
+    private Long height; // in cm
+    private double weight; // kg
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Patient patient = (Patient) o;
-        return Objects.equals(id, patient.id) && Objects.equals(userId, patient.userId) && Objects.equals(name, patient.name) && Objects.equals(lastName, patient.lastName) && gender == patient.gender && Objects.equals(birthDate, patient.birthDate) && Objects.equals(symptoms, patient.symptoms);
+        return Double.compare(patient.weight, weight) == 0 && Objects.equals(id, patient.id) && Objects.equals(userId, patient.userId) && Objects.equals(name, patient.name) && Objects.equals(lastName, patient.lastName) && gender == patient.gender && Objects.equals(birthDate, patient.birthDate) && Objects.equals(symptoms, patient.symptoms) && Objects.equals(height, patient.height);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, userId, name, lastName, gender, birthDate, symptoms);
+        return Objects.hash(id, userId, name, lastName, gender, birthDate, symptoms, height, weight);
     }
 
     @Override
@@ -41,6 +43,8 @@ public class Patient {
                 ", gender=" + gender +
                 ", birthDate=" + birthDate +
                 ", symptoms=" + symptoms +
+                ", height=" + height +
+                ", weight=" + weight +
                 '}';
     }
 }
