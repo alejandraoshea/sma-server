d82cbe1d9c33fd080daade3048cce4a12c32d2dc
alejandraoshea <alejandra.osheafernandez@usp.ceu.es>
2025-10-16 09:15:50 +0200

    created classes measurementsession and signal to store the signals from the patient

--- src/domain/MeasurementSession.java
+++ src/domain/MeasurementSession.java
@@ -1,36 +1,36 @@
 package domain;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Objects;
 
-public class PhysiologicalMeasurement {
+//** recording session grouping multiple signals
+public class MeasurementSession {
     private Integer id;
     private Long patientId;
-    private List<Signal> signals;
     private LocalDateTime timeStamp;
+    private List<Signal> signals;
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        PhysiologicalMeasurement that = (PhysiologicalMeasurement) o;
-        return Objects.equals(id, that.id) && Objects.equals(patientId, that.patientId) && Objects.equals(signals, that.signals) && Objects.equals(timeStamp, that.timeStamp);
+        MeasurementSession that = (MeasurementSession) o;
+        return Objects.equals(id, that.id) && Objects.equals(patientId, that.patientId) && Objects.equals(timeStamp, that.timeStamp) && Objects.equals(signals, that.signals);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, patientId, signals, timeStamp);
+        return Objects.hash(id, patientId, timeStamp, signals);
     }
 
     @Override
     public String toString() {
-        return "PhysiologicalMeasurement{" +
+        return "MeasurementSession{" +
                 "id=" + id +
                 ", patientId=" + patientId +
-                ", signals=" + signals +
                 ", timeStamp=" + timeStamp +
+                ", signals=" + signals +
                 '}';
     }
 }
--- src/domain/Signal.java
+++ src/domain/Signal.java
@@ -1,23 +1,40 @@
 package domain;
 
-import jakarta.persistence.*;
 import lombok.Data;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Data
-@Entity
 public class Signal {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    private String patientId;
+    private Long patientId; //** patient
+    private Long measurementSessionId; //** FK measurementSession
     private LocalDateTime timestamp;
+    private SignalType signalType;
+    private String patientData;
 
-    @Enumerated(EnumType.STRING)
-    private SignalType type;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Signal signal = (Signal) o;
+        return Objects.equals(id, signal.id) && Objects.equals(patientId, signal.patientId) && Objects.equals(measurementSessionId, signal.measurementSessionId) && Objects.equals(timestamp, signal.timestamp) && signalType == signal.signalType && Objects.equals(patientData, signal.patientData);
+    }
 
-    @Lob
-    private String data;
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, patientId, measurementSessionId, timestamp, signalType, patientData);
+    }
 
+    @Override
+    public String toString() {
+        return "Signal{" +
+                "id=" + id +
+                ", patientId=" + patientId +
+                ", measurementSessionId=" + measurementSessionId +
+                ", timestamp=" + timestamp +
+                ", signalType=" + signalType +
+                ", patientData='" + patientData + '\'' +
+                '}';
+    }
 }
