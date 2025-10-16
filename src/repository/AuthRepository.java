fd8c2fc322ad5603b476c8d82638b397d4c9b923
alejandraoshea <alejandra.osheafernandez@usp.ceu.es>
2025-10-16 09:45:20 +0200

    initial structure

--- src/repository/AuthRepository.java
+++ src/repository/AuthRepository.java
@@ -1,5 +1,17 @@
 package repository;
 
 //! database access for users
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class AuthRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public AuthRepository(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    //** find user by email? save user?
 }
