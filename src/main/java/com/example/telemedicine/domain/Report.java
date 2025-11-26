8604736c18e254fe5eaa06985c160bd350c435a0
alejandraoshea <alejandra.osheafernandez@usp.ceu.es>
2025-11-26 15:29:58 +0100

    lombok in report

--- src/main/java/com/example/telemedicine/domain/Report.java
+++ src/main/java/com/example/telemedicine/domain/Report.java
@@ -1,7 +1,12 @@
 package com.example.telemedicine.domain;
 
-import java.time.LocalDateTime;
+import lombok.Data;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Objects;
+
+@Data
 public class Report {
 
     private Long reportId;
@@ -13,9 +18,6 @@ public class Report {
     private String fileType;
     private LocalDateTime createdAt = LocalDateTime.now();
 
-    public Report() {
-    }
-
     public Report(Long patient_id, Long doctor_id, Long session_id, byte[] fileData, String fileName, String fileType) {
         this.patient_id = patient_id;
         this.doctor_id = doctor_id;
@@ -25,4 +27,32 @@ public class Report {
         this.fileType = fileType;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Report report = (Report) o;
+        return Objects.equals(reportId, report.reportId) && Objects.equals(patient_id, report.patient_id) && Objects.equals(doctor_id, report.doctor_id) && Objects.equals(session_id, report.session_id) && Arrays.equals(fileData, report.fileData) && Objects.equals(fileName, report.fileName) && Objects.equals(fileType, report.fileType) && Objects.equals(createdAt, report.createdAt);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(reportId, patient_id, doctor_id, session_id, fileName, fileType, createdAt);
+        result = 31 * result + Arrays.hashCode(fileData);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Report{" +
+                "reportId=" + reportId +
+                ", patient_id=" + patient_id +
+                ", doctor_id=" + doctor_id +
+                ", session_id=" + session_id +
+                ", fileData=" + Arrays.toString(fileData) +
+                ", fileName='" + fileName + '\'' +
+                ", fileType='" + fileType + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
 }
